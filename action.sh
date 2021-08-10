abb49f53127e79ec9a566d32c1a2ba17f2ff3b2e
xperia3 <88239736+xperia3@users.noreply.github.com>
2021-08-10 10:22:16 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
-branch=main
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
+#huehue
+git clone https://github.com/TeamDaisyX/DaisyXMusic /root/DaisyXMusic
+cp ultroid/.env /root/DaisyXMusic/.env
+cd /root/DaisyXMusic
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
